09eae9a69c0e3e14e416a0733483ecad79a27105
Matt Rix <matt@matt-rix.com>
2013-05-10 23:17:06 -0400

    Input manager megafication

--- Unity/Assets/Scripts/Tether/Utils/GamepadManager.cs
+++ Unity/Assets/Scripts/Tether/Utils/GamepadManager.cs
@@ -16,7 +16,11 @@ public class GamepadManager
 
     public GamepadManager()
     {
+		GenerateInputManagerData ();
+
+
         Update();
+
     }
 
     public Gamepad GetGamepad(int index)
@@ -60,5 +64,53 @@ public class GamepadManager
             gamepads[g].Update();
         }
     }
+
+	private static string TAB_CHAR = "  ";
+
+	void GenerateInputManagerData ()
+	{
+		string fullOutput = "%YAML 1.1\n";
+		fullOutput += "%TAG !u! tag:unity3d.com,2011:\n";
+		fullOutput += "--- !u!13 &1\n";
+		fullOutput += "InputManager:\n";
+		fullOutput += TAB_CHAR+"m_ObjectHideFlags: 0\n";
+		fullOutput += TAB_CHAR+"m_Axes:\n";
+
+		for (int j = 0; j<11; j++) 
+		{
+			string output = "";
+			for(int a = 0; a<10; a++)
+			{
+				output += GetAxisInputManagerData(j+1,0,"Axis "+(a+1),a);
+			}
+
+			fullOutput += output;
+		}
+
+		System.IO.File.WriteAllText("Assets/Notes/InputManagerData.txt", fullOutput);
+		Debug.Log(fullOutput);
+	}
+
+	string GetAxisInputManagerData(int ordinal, int invert, string axisName, int axisIndex)
+	{
+		string output = TAB_CHAR+"- serializedVersion: 3\n";
+		output += TAB_CHAR+TAB_CHAR+"m_Name: Joystick "+ordinal.ToString()+" "+axisName+"\n";
+		output += TAB_CHAR+TAB_CHAR+"descriptiveName:\n";
+		output += TAB_CHAR+TAB_CHAR+"descriptiveNegativeName:\n";
+		output += TAB_CHAR+TAB_CHAR+"negativeButton:\n";
+		output += TAB_CHAR+TAB_CHAR+"positiveButton:\n";
+		output += TAB_CHAR+TAB_CHAR+"altNegativeButton:\n";
+		output += TAB_CHAR+TAB_CHAR+"altPositiveButton:\n";
+		output += TAB_CHAR+TAB_CHAR+"gravity: 3\n";
+		output += TAB_CHAR+TAB_CHAR+"dead: .00100000005\n";
+		output += TAB_CHAR+TAB_CHAR+"sensitivity: 1\n";
+		output += TAB_CHAR+TAB_CHAR+"snap: 0\n";
+		output += TAB_CHAR+TAB_CHAR+"invert: " + invert.ToString() + "\n";
+		output += TAB_CHAR+TAB_CHAR+"type: 2" + "\n";
+		output += TAB_CHAR+TAB_CHAR+"axis: " + axisIndex.ToString() + "\n";
+		output += TAB_CHAR+TAB_CHAR+"joyNum: " +ordinal.ToString() + "\n";
+
+		return output;
+	}
 }
 
