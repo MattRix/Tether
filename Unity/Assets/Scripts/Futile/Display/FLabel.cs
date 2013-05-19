d8433f01a008ae2d67ff041203ba121cdbeda1c1
Matt Rix <matt@matt-rix.com>
2013-05-19 16:05:58 -0400

    Fixing FLabel

--- Unity/Assets/Scripts/Futile/Display/FLabel.cs
+++ Unity/Assets/Scripts/Futile/Display/FLabel.cs
@@ -9,96 +9,84 @@ public class FLabel : FFacetElementNode
 {
 	public static float defaultAnchorX = 0.5f;
 	public static float defaultAnchorY = 0.5f;
-	
+
 	protected FFont _font;
 	protected string _fontName;
 	protected string _text;
-	
+
 	protected Color _color = Futile.white;
 	protected Color _alphaColor = Futile.white;
-	
+
 	protected FLetterQuadLine[] _letterQuadLines;
-	
+
 	protected bool _isMeshDirty = false;
-	
+
 	protected float _anchorX = defaultAnchorX;
 	protected float _anchorY = defaultAnchorY;
-	
+
 	protected bool _doesTextNeedUpdate = false;
 	protected bool _doesLocalPositionNeedUpdate = false;
 	protected bool _doQuadsNeedUpdate = false;
-	
+
 	protected Rect _textRect;
-	
+
 	protected FTextParams _textParams;
-	
+
 	public FLabel (string fontName, string text) : this(fontName, text, new FTextParams())
 	{
 	}
-	
+
 	public FLabel (string fontName, string text, FTextParams textParams) : base()
 	{
 		_fontName = fontName;
 		_text = text;
 		_font = Futile.atlasManager.GetFontWithName(_fontName);
 		_textParams = textParams;
-		 
+
 		Init(FFacetType.Quad, _font.element, 0);
-		
+
 		CreateTextQuads();
 	}
-	
-	override public void HandleAddedToStage()
-	{
-		Futile.instance.SignalUpdate += HandleUpdate;
-		base.HandleAddedToStage();
-	}
-	
-	override public void HandleRemovedFromStage()
-	{
-		Futile.instance.SignalUpdate -= HandleUpdate;
-		base.HandleRemovedFromStage();
-	}
-	
+
 	public void CreateTextQuads()
 	{
 		_doesTextNeedUpdate = false;
-		
+
 		int oldFacetsNeeded = _numberOfFacetsNeeded;
-		
+
 		_letterQuadLines = _font.GetQuadInfoForText(_text,_textParams);
-		
+
 		_numberOfFacetsNeeded = 0;
-		
+
 		int lineCount = _letterQuadLines.Length;
 		for(int i = 0; i< lineCount; i++)
 		{
 			_numberOfFacetsNeeded += _letterQuadLines[i].quads.Length;
 		}
-		
+
 		if(_isOnStage)
 		{
 			int delta = _numberOfFacetsNeeded - oldFacetsNeeded;
-			
+
 			if(delta != 0) //if the number of letter quads has changed, tell the stage
 			{
 				_stage.HandleFacetsChanged();
 			}
 		}
-		
+
 		UpdateLocalPosition(); //figures out the bounds and alignment, and sets the mesh dirty
 	}
-	
+
 	public void UpdateLocalPosition()
 	{
 		_doesLocalPositionNeedUpdate = false;
-		
+
 		float minY = 100000000;
 		float maxY = -100000000;
-		
+
 		float minX = 100000000;
 		float maxX = -100000000;
-		
+
 		int lineCount = _letterQuadLines.Length;
 		for(int i = 0; i<lineCount; i++)
 		{
@@ -106,17 +94,17 @@ public class FLabel : FFacetElementNode
 			minY = Math.Min (line.bounds.yMin,minY);
 			maxY = Math.Max (line.bounds.yMax,maxY);
 		}
-		
+
 		float offsetY = -(minY + ((maxY-minY)*_anchorY));
-		
+
 		for(int i = 0; i<lineCount; i++)
 		{
 			FLetterQuadLine line = _letterQuadLines[i];
 			float offsetX = -line.bounds.width*_anchorX;
-			
+
 			minX = Math.Min (offsetX,minX);
 			maxX = Math.Max (offsetX+line.bounds.width,maxX);
-			
+
 			int quadCount = line.quads.Length;
 			for(int q = 0; q< quadCount; q++)
 			{
@@ -125,40 +113,32 @@ public class FLabel : FFacetElementNode
 				line.quads[q].CalculateVectors(offsetX+_font.offsetX+1.0f, offsetY+_font.offsetY+1.0f);
 			}
 		}
-		
+
 		_textRect.x = minX;
 		_textRect.y = minY+offsetY;
 		_textRect.width = maxX-minX;
 		_textRect.height = maxY-minY;
-		
+
 		_isMeshDirty = true; 
 	}
-	
-	private void HandleUpdate()
-	{
-		if(_doesTextNeedUpdate)
-		{
-			CreateTextQuads();
-		}		
-	}
-
+	 
 	override public void Redraw(bool shouldForceDirty, bool shouldUpdateDepth)
 	{
 		bool wasMatrixDirty = _isMatrixDirty;
 		bool wasAlphaDirty = _isAlphaDirty;
-		
+
 		UpdateDepthMatrixAlpha(shouldForceDirty, shouldUpdateDepth);
-		
+
 		if(shouldUpdateDepth)
 		{
 			UpdateFacets();
 		}
-		
+
 		if(wasMatrixDirty || shouldForceDirty || shouldUpdateDepth)
 		{
 			_isMeshDirty = true;
 		}
-		
+
 		if(wasAlphaDirty || shouldForceDirty)
 		{
 			_isMeshDirty = true;
@@ -169,85 +149,85 @@ public class FLabel : FFacetElementNode
 		{
 			UpdateLocalPosition();	
 		}
-		
+
 		if(_isMeshDirty)
 		{
 			PopulateRenderLayer();
 		}
 	}
-	
+
 	override public void PopulateRenderLayer()
 	{
 		if(_isOnStage && _firstFacetIndex != -1)
 		{
 			_isMeshDirty = false;
-			
+
 			Vector3[] vertices = _renderLayer.vertices;
 			Vector2[] uvs = _renderLayer.uvs;
 			Color[] colors = _renderLayer.colors;
-			
+
 			int vertexIndex0 = _firstFacetIndex*4;
 			int vertexIndex1 = vertexIndex0 + 1;
 			int vertexIndex2 = vertexIndex0 + 2;
 			int vertexIndex3 = vertexIndex0 + 3;
-			
+
 			int lineCount = _letterQuadLines.Length;
 			for(int i = 0; i<lineCount; i++)
 			{
 				FLetterQuad[] quads = _letterQuadLines[i].quads;
-				
-				
+
+
 				int quadCount = quads.Length;
-				
+
 				for(int q = 0; q<quadCount; q++)
 				{
 					FLetterQuad quad = quads[q];
 					FCharInfo charInfo = quad.charInfo;
-					
+
 					_concatenatedMatrix.ApplyVector3FromLocalVector2(ref vertices[vertexIndex0], quad.topLeft,0);
 					_concatenatedMatrix.ApplyVector3FromLocalVector2(ref vertices[vertexIndex1], quad.topRight,0);
 					_concatenatedMatrix.ApplyVector3FromLocalVector2(ref vertices[vertexIndex2], quad.bottomRight,0);
 					_concatenatedMatrix.ApplyVector3FromLocalVector2(ref vertices[vertexIndex3], quad.bottomLeft,0);
-					
+
 					uvs[vertexIndex0] = charInfo.uvTopLeft;
 					uvs[vertexIndex1] = charInfo.uvTopRight;
 					uvs[vertexIndex2] = charInfo.uvBottomRight;
 					uvs[vertexIndex3] = charInfo.uvBottomLeft;
-					
+
 					colors[vertexIndex0] = _alphaColor;
 					colors[vertexIndex1] = _alphaColor;
 					colors[vertexIndex2] = _alphaColor;
 					colors[vertexIndex3] = _alphaColor;
-					
+
 					vertexIndex0 += 4;
 					vertexIndex1 += 4;
 					vertexIndex2 += 4;
 					vertexIndex3 += 4;
 				}
 			}
-			
+
 			_renderLayer.HandleVertsChange();
 		}
 	}
 
-    public FLabelAlignment alignment
-    {
-        get 
-        {
-            if (_anchorX == 0.5f) return FLabelAlignment.Center;
-            if (_anchorX == 0.0f) return FLabelAlignment.Left;
-            if (_anchorX == 1.0f) return FLabelAlignment.Right;
+	public FLabelAlignment alignment
+	{
+		get 
+		{
+			if (_anchorX == 0.5f) return FLabelAlignment.Center;
+			if (_anchorX == 0.0f) return FLabelAlignment.Left;
+			if (_anchorX == 1.0f) return FLabelAlignment.Right;
 
-            return FLabelAlignment.Custom;
-        }
-        set 
-        {
-            if (value == FLabelAlignment.Center) this.anchorX = 0.5f;
-            else if (value == FLabelAlignment.Left) this.anchorX = 0.0f;
-            else if (value == FLabelAlignment.Right) this.anchorX = 1.0f;
-        }
-    }
-	
+			return FLabelAlignment.Custom;
+		}
+		set 
+		{
+			if (value == FLabelAlignment.Center) this.anchorX = 0.5f;
+			else if (value == FLabelAlignment.Left) this.anchorX = 0.0f;
+			else if (value == FLabelAlignment.Right) this.anchorX = 1.0f;
+		}
+	}
+
 	public string text
 	{
 		get {return _text;}
@@ -257,10 +237,11 @@ public class FLabel : FFacetElementNode
 			{
 				_text = value; 
 				_doesTextNeedUpdate = true;
+				CreateTextQuads();
 			}
 		}
 	}
-	
+
 	public float anchorX
 	{
 		get {return _anchorX;}
@@ -299,10 +280,15 @@ public class FLabel : FFacetElementNode
 			}
 		}
 	}
-	
+
 	virtual public Rect textRect
 	{
-		get {return _textRect;}	
+		get 
+		{
+			if(_doesTextNeedUpdate) CreateTextQuads();
+			if(_doesLocalPositionNeedUpdate) UpdateLocalPosition();
+			return _textRect;
+		}	
 	}
 
 	[Obsolete("FLabel's boundsRect is obsolete, use textRect instead")]
@@ -310,20 +296,20 @@ public class FLabel : FFacetElementNode
 	{
 		get {throw new NotSupportedException("boundsRect is obsolete! Use textRect instead");}
 	}
-	
+
 	//for convenience
 	public void SetAnchor(float newX, float newY)
 	{
 		this.anchorX = newX;
 		this.anchorY = newY;
 	}
-	
+
 	public void SetAnchor(Vector2 newAnchor)
 	{
 		this.anchorX = newAnchor.x;
 		this.anchorY = newAnchor.y;
 	}
-	
+
 	public Vector2 GetAnchor()
 	{
 		return new Vector2(_anchorX,_anchorY);	
@@ -332,10 +318,10 @@ public class FLabel : FFacetElementNode
 
 public enum FLabelAlignment
 {
-    Center,
-    Left,
-    Right,
-    Custom
+	Center,
+	Left,
+	Right,
+	Custom
 }
 
 
